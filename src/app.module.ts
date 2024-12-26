662745cca30334e13605613539d992cf0286d0f7
David <davidagamez04@gmail.com>
2024-12-26 15:03:18 -0500

    feat: configure TypeORM with MongoDB and integrate CommentsModule into the main application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CommentsModule } from './comments/comments.module';
 
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      host: 'interactions-database',
+      port: 27017,
+      database: 'interactionsdb',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    CommentsModule,
+  ],
+})
 export class AppModule {}
