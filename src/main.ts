c26b139ab16cd740a10993e997a6d87df2a64077
David <davidagamez04@gmail.com>
2024-12-26 15:03:10 -0500

    feat: refactor main application to use NATS microservice transport and add global validation pipes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,36 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { type MicroserviceOptions, Transport } from '@nestjs/microservices';
+
 import { AppModule } from './app.module';
+import { envs } from './config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const logger = new Logger('Interactions.ms');
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.NATS,
+      options: {
+        servers: envs.natsServers,
+      },
+    },
+  );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      exceptionFactory: (errors) => {
+        errors.map((e) => {
+          logger.error(e.constraints);
+        });
+      },
+    }),
+  );
+
+  await app.listen();
+  logger.log(`running on port ${envs.port}`);
 }
+
 bootstrap();
